29613fe34399e4730315068e52958fea94b1d4a4
yassCan <89553163+yassCan@users.noreply.github.com>
2021-11-16 17:00:57 +0100

    Add files via upload

---
+++ ElementJS/element.js
@@ -0,0 +1,136 @@
+const { pow } = Math
+const { floor } = Math
+const { head } = document
+const { body } = document
+const print = console.log
+const loop = setInterval
+const sleep = setTimeout
+const html = document.getElementsByTagName('html')[0]
+// class ElementJS {
+// 	constructor() {}
+// 	rerender(component) {
+// 		this.clonedElement = component.cloneNode(true)
+// 		component.innerHTML = ""
+// 		component.remove()
+// 		if(component.parent == undefined) {
+// 			body.append(this.clonedElement)
+// 		}
+// 	}
+// }
+class Element {
+  constructor(name, att) {
+  	// atts => attributes
+    this.atts = att
+    this.name = name
+    this.keys = Object.keys(this.atts)
+    this.keyValues = Object.values(this.atts)
+    // newAtt => new attribute
+    this.newAtt;
+    this.styles
+    // elem => element
+    let elem = document.createElement(this.name)
+    let parent
+    this.keys.forEach((key, i) => {
+    	// val => value
+	  let val = this.keyValues[i]
+	  if(key == "css") {
+	  	this.setStyle(val, elem)
+	  } else if (key == "childs") {
+	  	this.append(val, elem)
+	  } else if(key == "text") {
+	  	this.setText(val, elem)
+	  } else if(key == "parent") {
+			val.append(elem)
+		}
+	  this.newAtt = document.createAttribute(key)
+	  this.newAtt.value = val
+	  elem.setAttributeNode(this.newAtt)
+    });
+    return elem
+  }
+  setText(val, elem) {
+  	elem.textContent = val
+  }
+  setStyle(styles, elem) {
+  	this.styles = styles
+  	for(const prop in this.styles) {
+  		elem.style[prop] = this.styles[prop]
+  	}
+  }
+  append(childs, elem) {
+  	for(const child in childs) {
+  		elem.append(childs[child])
+  		let parentAtt = document.createAttribute('parent')
+  		parentAtt.value = elem.toString()
+  		childs[child].setAttributeNode(parentAtt)
+  	}
+  }
+}
+
+function createStyle(styles) {
+	if(styles != undefined) {
+		this.styles = styles
+		this.camelPropsObj = {}
+		this.props = Object.keys(this.styles)
+		this.keyValues = Object.values(this.styles)
+		this.camelProps = []
+		this.props.forEach((prop) => {
+			this.camelProps.push(toCamelCase(prop))
+		})
+		this.camelProps.forEach((prop, i) => {
+			this.camelPropsObj[prop] = this.keyValues[i]
+		})
+		return this.camelPropsObj
+	}
+}
+const element = new ElementJS()
+function toCamelCase(prop) {
+	// prop => properti, newProp => new propertie
+	let newProp
+	for(let i = 0;i < prop.length;i++) {
+		if(prop[i] == "-") {
+			newProp = prop.replace(/\-[a-z]/, prop[i+1].toUpperCase())
+		}
+	}
+	newProp = prop
+	return newProp
+}
+
+function select(selector) {
+	return document.querySelector(selector)
+}
+
+function selectAll(selector) {
+	return document.querySelectorAll(selector)
+}
+
+function Request() {
+	this.post = (url, data, options) => {
+		let methodOptions = options ?? {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(data)
+			}
+		return fetch(url, methodOptions)
+	}
+	this.get = (url ,options) => {
+		let methodOptions = options ?? {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}
+		return fetch(url, methodOptions)
+	}
+}
+const request = new Request()
+
+function random(min, max) {
+	return Math.random()*(max-min)+min
+}
+
+function $(func) {
+	func()
+}
